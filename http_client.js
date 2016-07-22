ae803585c230456f7e4218dee152625b099d16b2
rinoale <rinoale@hanmail.net>
2016-07-22 03:22:39 +0000

    with official solution

--- http_client.js
+++ http_client.js
@@ -1,1 +1,19 @@
 var http = require('http');
+
+
+http.get(process.argv[2], function(response) {
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    console.log(data);
+  });
+});
+
+//official solution
+//  var http = require('http')
+//
+//  http.get(process.argv[2], function (response) {
+//    response.setEncoding('utf8')
+//    response.on('data', console.log)
+//    response.on('error', console.error)
+//  }).on('error', console.error)
