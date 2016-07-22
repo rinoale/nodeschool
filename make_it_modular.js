2830a1745e7082df7bc090a33c31b55082aaebed
rinoale <rinoale@hanmail.net>
2016-07-22 02:48:13 +0000

    shorter version

--- make_it_modular.js
+++ make_it_modular.js
@@ -2,19 +2,13 @@ var fs = require('fs')
 var path = require('path')
 
 module.exports = function (folder, filter, callback){
-  // if (err) return console.error(err);
-  var ext = '.' + filter;
-
-  var pathArr = [];
-
   fs.readdir(folder, function (err, files) {
     if (err) return callback(err);
 
-    files.forEach(function(file) {
-      if (path.extname(file) === ext) {
-        pathArr.push(file);
-      }
-    });
+    pathArr = files.filter(function (file) {
+      return path.extname(file) === '.' + filter;
+    })
+
     callback(null, pathArr);
   });
 };
