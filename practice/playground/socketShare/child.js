bd55be29fee43848c833ea56d1b2f146e42c2638
rinoale <rinoale@hanmail.net>
2016-12-16 10:04:16 +0000

    pass socket between processes

--- practice/playground/socketShare/child.js
+++ practice/playground/socketShare/child.js
@@ -3,5 +3,7 @@ process.on('message', function (message, socket) {
   socket.on('data', function (data) {
     console.log('child : ' + data);
     socket.write('Echo from child : ' + data);
+
+    process.send('message', socket);
   })
 })
--- practice/playground/socketShare/parent.js
+++ practice/playground/socketShare/parent.js
@@ -10,11 +10,20 @@ server.on('connection', function (socket) {
 
   setTimeout(function () {
     child.send('socket', socket);
-  }, 10000);
+  }, 30000);
 
   server.getConnections(function (err, count) {
     console.log('Connections : ' + count);
   })
 });
+
+child.on('message', function (message, socket) {
+  console.log('Child sent : ' + message);
+
+  socket.on('data', function (data) {
+    console.log('parent2 : ' + data);
+    socket.write('Echo from parent2 : ' + data);
+  })
+})
 
 server.listen(1337);
